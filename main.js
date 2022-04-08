990f90d462d9c4d458f8c71dfa4863de5f8c4d85
hpNUTELLA <qoziboevdilshodbek@gmail.com>
2022-04-08 07:13:56 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+var confirmed = confirm('Sayohat qilmoqchimisiz')
+console.log(confirmed)
+var name = prompt('ismingiz')
+alert(name + 'Assalomu aleykum xush kelibsiz')
+if(confirmed) {
+    var money = prompt('Mablagingizni kiriting ')
+    if ( money >= 2000) {
+        alert('siz bu mablag bilan dubailga borolisiz')
+    }
+
+    else if ( money >= 1000) {
+        alert('siz bu mablag bilan dubailga borolisiz')
+    }
+
+    else if ( money >= 500) {
+        alert('siz bu mablag bilan Indiaga borolisiz')
+    }
+
+    else if ( money >= 100) {
+        alert('siz bu mablag bilantoshken borolisiz')
+    }
+}
